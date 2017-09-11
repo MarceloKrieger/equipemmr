f4cb18b15ff62b73f35021cdd26b75dabedc6275
MarceloKrieger <marcelogkrieger@gmail.com>
2017-09-11 16:01:26 -0300

    Update ExercicioStringUri02.java

--- src/uriString/ExercicioStringUri02.java
+++ src/uriString/ExercicioStringUri02.java
@@ -1,58 +1,178 @@
 package uriString;
 
 public class ExercicioStringUri02 {
-	int jogador1, jogador2;
-	String resultado;
-	int partidas;
-	public int getPartidas() {
-		return partidas;
-	}
-	public void setPartidas(int partidas) {
-		this.partidas = partidas;
-	}
-	public int getJogador1() {
-		return jogador1;
-	}
-	public void setJogador1(int jogador1) {
-		this.jogador1 = jogador1;
-	}
-	public int getJogador2() {
-		return jogador2;
-	}
-	public void setJogador2(int jogador2) {
-		this.jogador2 = jogador2;
-	}
-	public String getResultado() {
-		int pedra = 1, papel = 2, tesoura = 3, lagarto = 4, spock = 5;
-		for (int i = 0; i <= partidas; i++){
-			if (jogador1 == pedra){
-				if (jogador2 == pedra){
-					System.out.println("Resultado: EMPATE");
-				}
-					if (jogador2 == papel){
-						System.out.println("Resultado: JOGADOR2");
-					}
-						if (jogador2 == tesoura){
-							System.out.println("Resultado: JOGADOR1");
-						}
-							if (jogador2 == lagarto){
-								System.out.println("Resultado: JOGADOR1");
-							}		
-								if (jogador2 == spock){
-									System.out.println("Resultado: JOGADOR2");
-								}		
-			}
 	
+	int Pedra = 1,Papel = 2,Tesoura = 3,Lagarto = 4,Spock = 5 ,player1, player2,JOGO;
 
+	public int getPlayer1() {
+		return player1;
+	}
+
+	public void setPlayer1(int player1) {
+		this.player1 = player1;
+	}
+
+	public int getPlayer2() {
+
+		return player2;
+	}
+	
+	public void setPlayer2(int player2) {
+		this.player2 = player2;
+	}
+
+	public int getJOGO() {
+	
+	//----------------------------------- COMPARATIVO ENTRE PEDRA E PAPEL -------------------------------------------------------------//
 		
+		if(player1 == Pedra && player2 == Papel) {
+			System.out.print("Papel cobre Pedra!");
+			System.out.print("Player 2 wins!");
+			
 		}
-		return resultado;
+		if(player1 == Papel && player2 == Pedra) {
+			System.out.print("Papel cobre Pedra!");
+			System.out.print("Player 1 wins!");
+			
+		}
+		if(player1 == Pedra && player2 == Pedra || player1 == Papel && player2 == Papel) {
+			System.out.print("Empate!");
+		}
+		
+	//----------------------------------- COMPARATIVO ENTRE PEDRA E TESOURA -------------------------------------------------------------//
+		
+		if(player1 == Pedra && player2 == Tesoura) {
+			System.out.print("Pedra esmaga Tesoura!\n\n");
+			System.out.print("Player 1 wins!");
+			
+		}
+		if(player1 == Tesoura && player2 == Pedra) {
+			System.out.print("Pedra esmaga Tesoura!\n\n");
+			System.out.print("Player 2 wins!");
+		}
+		if(player1 == Pedra && player2 == Pedra || player1 == Tesoura && player2 == Tesoura) {
+			System.out.print("Empate!");
+		}
+		
+	//----------------------------------- COMPARATIVO ENTRE PEDRA E LAGARTO -------------------------------------------------------------//		
+		
+		if(player1 == Pedra && player2 == Lagarto) {
+			System.out.print("Pedra derruba Lagarto!\n\n");
+			System.out.print("Player 1 wins!");
+			
+		}
+		if(player1 == Lagarto && player2 == Pedra) {
+			System.out.print("Pedra derruba Lagarto!\n\n");
+			System.out.print("Player 2 wins!");
+		}
+		if(player1 == Pedra && player2 == Pedra || player1 == Lagarto && player2 == Lagarto) {
+			System.out.print("Empate!");
+		}
+		
+	//----------------------------------- COMPARATIVO ENTRE PEDRA E SPOCK -------------------------------------------------------------//
+		
+		if(player1 == Pedra && player2 == Spock) {
+			System.out.print("Spock vaporiza Pedra!\n\n");
+			System.out.print("Player 2 wins!");
+			
+		}
+		if(player1 == Spock && player2 == Pedra) {
+			System.out.print("Spock vaporiza Pedra!\n\n");
+			System.out.print("Player 1 wins!");
+		}
+		if(player1 == Pedra && player2 == Pedra || player1 == Spock && player2 == Spock) {
+			System.out.print("Empate!");
+		}
+		
+	//----------------------------------- COMPARATIVO ENTRE PAPEL E TESOURA -------------------------------------------------------------//
+		
+		if(player1 == Papel && player2 == Tesoura) {
+			System.out.print("Tesoura corta Papel!\n\n");
+			System.out.print("Player 2 wins!");
+			
+		}
+		if(player1 == Tesoura && player2 == Papel) {
+			System.out.print("Tesoura corta Papel!\n\n");
+			System.out.print("Player 1 wins!");
+		}
+		if(player1 == Papel && player2 == Papel || player1 == Tesoura && player2 == Tesoura) {
+			System.out.print("Empate!");
+		}
+		
+	//----------------------------------- COMPARATIVO ENTRE PAPEL E LAGARTO -------------------------------------------------------------//
+		
+		if(player1 == Papel && player2 == Lagarto) {
+			System.out.print("Lagarto come Papel!\n\n");
+			System.out.print("Player 2 wins!");
+			
+		}
+		if(player1 == Lagarto && player2 == Papel) {
+			System.out.print("Lagarto come Papel!\n\n");
+			System.out.print("Player 1 wins!");
+		}
+		if(player1 == Papel && player2 == Papel || player1 == Lagarto && player2 == Lagarto) {
+			System.out.print("Empate!");
+		}
+		
+	//----------------------------------- COMPARATIVO ENTRE PAPEL E SPOCK -------------------------------------------------------------//
+		
+		if(player1 == Papel && player2 == Spock) {
+			System.out.print("Papel refuta Spock!\n\n");
+			System.out.print("Player 1 wins!");
+			
+		}
+		if(player1 == Spock && player2 == Papel) {
+			System.out.print("Papel refuta Spock!\n\n");
+			System.out.print("Player 2 wins!");
+		}
+		if(player1 == Papel && player2 == Papel || player1 == Spock && player2 == Spock) {
+			System.out.print("Empate!");
+		}
+		
+	//----------------------------------- COMPARATIVO ENTRE TESOURA E LAGARTO -------------------------------------------------------------//
+		
+		if(player1 == Tesoura && player2 == Lagarto) {
+			System.out.print("Tesoura prende Lagarto!\n\n");
+			System.out.print("Player 1 wins!");
+			
+		}
+		if(player1 == Lagarto && player2 == Tesoura) {
+			System.out.print("Tesoura prende Lagarto!\n\n");
+			System.out.print("Player 2 wins!");
+		}
+		if(player1 == Tesoura && player2 == Tesoura || player1 == Lagarto && player2 == Lagarto) {
+			System.out.print("Empate!");
+		}	
+		
+	//----------------------------------- COMPARATIVO ENTRE TESOURA E SPOCK -------------------------------------------------------------//
+		
+		if(player1 == Tesoura && player2 == Spock) {
+			System.out.print("Spock derrete Tesoura!\n\n");
+			System.out.print("Player 2 wins!");
+			
+		}
+		if(player1 == Spock && player2 == Tesoura) {
+			System.out.print("Spock derrete Tesoura!\n\n");
+			System.out.print("Player 1 wins!");
+		}
+		if(player1 == Tesoura && player2 == Tesoura || player1 == Spock && player2 == Spock) {
+			System.out.print("Empate!");
+		}	
+		
+	//----------------------------------- COMPARATIVO ENTRE LAGARTO E SPOCK -------------------------------------------------------------//	
+		
+		if(player1 == Lagarto && player2 == Spock) {
+			System.out.print("Lagarto adormece Spock!\n\n");
+			System.out.print("Player 1 wins!");
+			
+		}
+		if(player1 == Spock && player2 == Lagarto) {
+			System.out.print("Lagarto adormece Spock!\n\n");
+			System.out.print("Player 2 wins!");
+		}
+		if(player1 == Lagarto && player2 == Lagarto || player1 == Spock && player2 == Spock) {
+			System.out.print("Empate!");
+		}	
+		return JOGO;
 	}
 }
-
-
-
-
-
-
-
