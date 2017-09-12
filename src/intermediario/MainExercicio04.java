b121da665ba3375044cfcd46c512bce194f171ca
MarceloKrieger <marcelogkrieger@gmail.com>
2017-09-11 21:50:01 -0300

    Update MainExercicio04.java

--- src/intermediario/MainExercicio04.java
+++ src/intermediario/MainExercicio04.java
@@ -9,12 +9,9 @@ public class MainExercicio04 {
 		Scanner teclado = new Scanner(System.in);
 		Exercicio04 binario = new Exercicio04();
 		
-		System.out.print("Digite um número inteiro e positivo: "); 
+		System.out.print("Digite um nÃºmero inteiro e positivo: "); 
 		binario.setEntrada(teclado.nextInt());
-		//System.out.println(binario.getDecimalParaBinario());	
-		LinkedList<Integer> lista = binario.getDecimalParaBinario();
-		for (Integer valor : lista){
-			System.out.println(valor);
-		}
+			System.out.print( binario.getDecimalParaBinario());
+		
 	}
 }
