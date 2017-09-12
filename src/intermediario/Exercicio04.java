501050301ef51da5c9e13dcdd6a569990848c701
MarceloKrieger <marcelogkrieger@gmail.com>
2017-09-11 21:50:58 -0300

    Update Exercicio04.java

--- src/intermediario/Exercicio04.java
+++ src/intermediario/Exercicio04.java
@@ -1,28 +1,44 @@
 package intermediario;
-
-import java.util.LinkedList;
-import java.util.Scanner;
+/**
+ * Converte um número decimal para binário
+ * @author marcelo.krieger
+ *
+ */
 public class Exercicio04 {
 	
-	int entrada,aux,resultado, decimalParaBinario;
+	private int entrada,aux = 1,resultado, decimalParaBinario;
 	
+	/**
+	 * Retorna o valor que será convertido
+	 * @return
+	 */
 	public int getEntrada() {
 		return entrada;
 	}
 
+	/**
+	 * Define o valor que será convertido
+	 * @param entrada
+	 */
 	public void setEntrada(int entrada) {
 		this.entrada = entrada;
 	}
 
-	public LinkedList<Integer> getDecimalParaBinario() {
-		LinkedList<Integer> retorno = new LinkedList<Integer>();
-		if(aux > 0) {	
-			aux = entrada % 2;
-			resultado = entrada / 2;
-			retorno.add( resultado );
+	/**
+	 * Executa divisões consecutivas para gerar um número binário
+	 * Usa a classe StringBuilder para evitar estouro de memória 
+	 * (que poderia ser causado pelo uso direto da classe string)
+	 * A reversão do valor em binário é feito pelo método reverse
+	 * do StringBuilder
+	 * @return
+	 */
+	public String getDecimalParaBinario() {
+		StringBuilder builder = new StringBuilder();
+		resultado = entrada;
+		while(resultado >= 1) {	
+			aux = resultado % 2;
+			builder.append(aux);
+			resultado = resultado / 2;
 		}
-		return retorno;
+		return builder.reverse().toString();
 	}
-	
-}
-
